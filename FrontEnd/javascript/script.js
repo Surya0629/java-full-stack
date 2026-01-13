750e76d47e67e4398c2af4cd77f406f1fc6e994d
Surya Pratap Singh <158257181+Surya0629@users.noreply.github.com>
2026-01-13 11:42:16 +0530

    start learning js

---
+++ FrontEnd/javascript/script.js
@@ -0,0 +1,17 @@
+var n3 = 2
+var n4 = `Hey there I am learning javascript and class ${n3}`
+console.log(n4)
+
+
+//boolean
+ var n5 = true
+ console.log(n5)//true
+ console.log(typeof(n5))// 
+ // if we do nested typeof of a data type it will return string
+ console.log(typeof typeof typeof n5)
+ // if we did not provide any value to a variable it return "unbdefined" given by js engine ,and these are falsy.. 
+ //..value undefimed and null
+ var n6
+ console.log(n6)
+ console.log(typeof n6)
+ console.log(typeof typeof n6)
